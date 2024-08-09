be71e886a9b8daa5c5862462b47a09e7d7bfe645
gorgemul <zeyang793@gmail.com>
2024-08-09 18:11:25 +0800

    commit kinda stinky post and put endpoints, need more refactor

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -54,7 +55,7 @@ func (s *Server) getHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	result, err := json.Marshal(todos)
+	result, err := json.MarshalIndent(todos, "", "    ")
 
 	if err != nil {
 		assertInternalErr(w, err)
@@ -70,11 +71,64 @@ func (s *Server) getHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) postHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from post"))
+	var newTodo types.NewTodo
+
+	err := json.NewDecoder(r.Body).Decode(&newTodo)
+	if err != nil {
+		assertBadRequest(w, err)
+		return
+	}
+
+	content := newTodo.Content
+
+	if len(content) == 0 {
+		http.Error(w, "content can't not be parsed!", http.StatusBadRequest)
+		return
+	}
+
+	_, err = s.db.Exec(context.Background(), "INSERT INTO todozz (content) VALUES ($1);", content)
+	if err != nil {
+		assertInternalErr(w, err)
+		return
+	}
+
+	fmt.Fprintf(w, "Successfully add %q", content)
 }
 
 func (s *Server) putHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from put"))
+	var updateTodo types.UpdateTodo
+
+	err := json.NewDecoder(r.Body).Decode(&updateTodo)
+	if err != nil {
+		assertBadRequest(w, err)
+		return
+	}
+
+	updateId := updateTodo.Id
+	updateContent := updateTodo.Content
+
+	if updateId <= 0 {
+		http.Error(w, "invlid id", http.StatusBadRequest)
+		return
+	}
+
+	if !todoExist(s.db, updateId) {
+		http.Error(w, "update todo is not exist!", http.StatusBadRequest)
+		return
+	}
+
+	if len(updateContent) == 0 {
+		http.Error(w, "update content is empty!", http.StatusBadRequest)
+		return
+	}
+
+	_, err = s.db.Exec(context.Background(), "UPDATE todozz SET content = $1 WHERE id = $2", updateContent, updateId)
+	if err != nil {
+		assertInternalErr(w, err)
+		return
+	}
+
+	fmt.Fprintf(w, "Successfully update id: %d!!", updateId)
 }
 
 func (s *Server) deleteHandler(w http.ResponseWriter, r *http.Request) {
@@ -84,4 +138,14 @@ func (s *Server) deleteHandler(w http.ResponseWriter, r *http.Request) {
 func assertInternalErr(w http.ResponseWriter, err error) {
 	http.Error(w, err.Error(), http.StatusInternalServerError)
 	log.Println(err)
+}
+
+func assertBadRequest(w http.ResponseWriter, err error) {
+	http.Error(w, err.Error(), http.StatusBadRequest)
+	log.Println(err)
+}
+
+func todoExist(db *pgx.Conn, id int) bool {
+	err := db.QueryRow(context.Background(), "SELECT * FROM todozz WHERE id=$1", id).Scan(&id)
+	return err != pgx.ErrNoRows
 }
--- types/todo.go
+++ types/todo.go
@@ -7,3 +7,12 @@ type Todo struct {
 	Content   string    `json:"content"`
 	CreatedAt time.Time `json:"createdAt"`
 }
+
+type NewTodo struct {
+	Content string
+}
+
+type UpdateTodo struct {
+	Id      int
+	Content string
+}
